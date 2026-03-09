b1ae51631fef88c7f8602f72df2123c81f82ee09
EuLo C. <eulysiscuerdo03@gmail.com>
2026-03-09 10:07:25 +0800

    Eulysis branch (#5)
    * Inbound/Outbound Kiosk
    
    Attemp Merge 1
    
    * Inbound/Outbound Kiosk
    
    Attempt Merge 2
    
    * I Frame attendances are set.
    
    * Update InboundOutboundKioskModule.tsx
    
    * I frame attendances are completely done
    
    * Added Fallback IP's in Attendance Modules, and Added Auto Refresh Modal when server is down in all pages

--- src/app/(kiosk-management)/kiosk-management/vertex-attendance/page.tsx
+++ src/app/(kiosk-management)/kiosk-management/vertex-attendance/page.tsx
@@ -1,9 +1,10 @@
 import { VertexAttendanceModule } from "@/modules/kiosk-management/vertex-attendance/VertexAttendanceModule";
 
 export default function VertexAttendancePage() {
-    const url = process.env.VERTEX_ATTENDANCE_URL || process.env.VERTEX_ATTENDANCE_URL_VPN;
+    const url = process.env.VERTEX_ATTENDANCE_URL;
+    const fallbackUrl = process.env.VERTEX_ATTENDANCE_URL_VPN;
 
     return (
-        <VertexAttendanceModule url={url} />
+        <VertexAttendanceModule url={url} fallbackUrl={fallbackUrl} />
     );
 }
